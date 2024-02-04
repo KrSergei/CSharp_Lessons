610f80ae7931755e628a8c5addda9be5ed66b6f0
KrSergei <34679066+KrSergei@users.noreply.github.com>
2024-02-04 09:02:52 +0300

    add a few veriables

--- Lesson1/Task1/Program.cs
+++ Lesson1/Task1/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Hello, World!");
+
+
+int number_one = 100;
+double number_two = 5.55;
+char c = '!';
+bool b = true;
+
+Console.WriteLine(number_one);
+Console.WriteLine(number_two);
+Console.WriteLine(c);
+Console.WriteLine(b);
