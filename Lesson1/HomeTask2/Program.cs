cadfae4e790853fa8c331a17dafa8622938551ff
KrSergei <34679066+KrSergei@users.noreply.github.com>
2024-02-08 22:27:09 +0300

    Add Leson1:HomeTask2

---
+++ Lesson1/HomeTask2/Program.cs
@@ -0,0 +1,7 @@
+﻿/// Task1
+int value;
+Console.WriteLine("Enter numver");
+value = Convert.ToInt32(Console.ReadLine());
+if(value % 7 == 0 && value% 23 == 0) Console.WriteLine("the number is a multiple 7 and 23");
+else Console.WriteLine("the number is a not multiple 7 and 23");
+Console.ReadKey();
