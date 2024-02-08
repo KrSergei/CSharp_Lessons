7733bd883a5d1e60285807ed6c5621d8fe999ba9
KrSergei <34679066+KrSergei@users.noreply.github.com>
2024-02-08 23:02:40 +0300

    add Lesson1:HomeTask2:Task3

--- Lesson1/HomeTask2/Program.cs
+++ Lesson1/HomeTask2/Program.cs
@@ -1,21 +1,31 @@
 ﻿/// Task1
-float value;
-Console.WriteLine("Enter numver");
-value = Convert.ToInt32(Console.ReadLine());
-if(value % 7 == 0 && value % 23 == 0) Console.WriteLine("the number is a multiple 7 and 23");
-else Console.WriteLine("the number is a not multiple 7 and 23");
-Console.ReadKey();
+// decimal value;
+// Console.WriteLine("Enter numver");
+// value = Convert.ToInt32(Console.ReadLine());
+// if(value % 7 == 0 && value % 23 == 0) Console.WriteLine("the number is a multiple 7 and 23");
+// else Console.WriteLine("the number is a not multiple 7 and 23");
+// Console.ReadKey();
 
-///Task2 
-float x, y;
-Console.WriteLine("Enter x coordinate");
-x = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter y coordinate");
-y = Convert.ToInt32(Console.ReadLine());
-if( x >=0 && y >= 0) Console.WriteLine("Coordinate is in the first quarter");
-else if(x < 0 && y > 0) Console.WriteLine("Coordinate is in the second quarter");
-else if(x < 0 && y < 0) Console.WriteLine("Coordinate is in the third quarter");
-else if(x > 0 && y < 0) Console.WriteLine("Coordinate is in the fourth quarter");
-else if(x== 0 || y ==0) Console.WriteLine("Coordinate is on the axix");
+// ///Task2 
+// decimal x, y;
+// Console.WriteLine("Enter x coordinate");
+// x = Convert.ToDecimal(Console.ReadLine());
+// Console.WriteLine("Enter y coordinate");
+// y = Convert.ToDecimal(Console.ReadLine());
+// if( x >=0 && y >= 0) Console.WriteLine("Coordinate is in the first quarter");
+// else if(x < 0 && y > 0) Console.WriteLine("Coordinate is in the second quarter");
+// else if(x < 0 && y < 0) Console.WriteLine("Coordinate is in the third quarter");
+// else if(x > 0 && y < 0) Console.WriteLine("Coordinate is in the fourth quarter");
+// else if(x== 0 || y ==0) Console.WriteLine("Coordinate is on the axix");
 
 ///Task3
+int number;
+Console.WriteLine("Enter numver from 10 to 99");
+number = Convert.ToInt32(Console.ReadLine());
+while(number < 10 || number > 99){
+    Console.WriteLine("Not valid is a number");
+    Console.WriteLine("Enter number from 10 to 99");
+    number = Convert.ToInt32(Console.ReadLine());
+    }
+if(number / 10 > number % 10) Console.WriteLine(number / 10);
+else Console.WriteLine(number % 10);
