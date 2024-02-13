c75c8237a0ee39dd17664e35f3a745c34197d7a5
KrSergei <34679066+KrSergei@users.noreply.github.com>
2024-02-13 20:54:26 +0300

    Added Lesson2:HomeTask2-1

--- Lesson2/HomeTask2-1/Program.cs
+++ Lesson2/HomeTask2-1/Program.cs
@@ -1,11 +1,38 @@
-﻿void main(){
-  Console.WriteLine("Enter natural nimber from 1 to 100 000");
+﻿int number = 0;
+bool isCorrectNumber = false;
+
+void Main()
+{
+  while (isCorrectNumber == false)
+  {
+    Console.WriteLine("Enter natural number from 1 to 100 000");
+    number = GetNumber();
+    if (number > 0 && number < 100000) isCorrectNumber = true;
+  }
+  ConvertNumberToArray(number);
 }
 
+int GetNumber()
+{
+  return Convert.ToInt32(Console.ReadLine());
+}
 
-int GetNumber(){
-    
-} 
+void ConvertNumberToArray(int number)
+{
+  string num = number.ToString();
+  int[] array = new int[num.Length];
+  for (int i = 0; i < num.Length; i++)
+  {
+    array[i] = Convert.ToInt32(num[i]);
+    if (i == num.Length -1) PrintArrayValue(num[i], true);
+    else PrintArrayValue(num[i]);
+  }
+}
 
+void PrintArrayValue(char ch, bool lastChar = false)
+{
+  if (lastChar) Console.Write($"{ch}");
+  else Console.Write($"{ch}, ");
+}
 
 Main();
