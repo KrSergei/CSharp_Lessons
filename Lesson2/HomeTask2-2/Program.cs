49416aca93febc2cd4f226bb9e67153cefac0c55
KrSergei <34679066+KrSergei@users.noreply.github.com>
2024-02-17 23:21:50 +0300

    Added Lesson2:HomeTask2-2:Task2

--- Lesson2/HomeTask2-2/Program.cs
+++ Lesson2/HomeTask2-2/Program.cs
@@ -1,4 +1,8 @@
-﻿#region TASK1
+﻿using System.Diagnostics.CodeAnalysis;
+
+/// TASK-1: --- Программа бесконечно запрашивает целые числа с консоли. Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+/// TASK-2: --- Массив заполненный случайными трёхзначными числами. Программа показывает количество чётных чисел в массиве.
+/// NASK-3: --- Программа перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
 void Main()
 {
     /// <-------------- TASK1 -------------->    
@@ -16,7 +20,15 @@ void Main()
         if (isCanContinue) PrintToConsole("Sum numbers = " + sum);
         else PrintToConsole("You are enter q or sum numbers is a entered number is a even");
     }
+    /// <-------------- TASK2 -------------->
+    int[] randomNumbers = new int[10];
+    int countEvenNumbers = 0;
+    randomNumbers = CreateArray(randomNumbers.Length);
+    countEvenNumbers = CountEvenNumbers(randomNumbers);
+    PrintArrayToConsole(randomNumbers);
+    PrintToConsole(countEvenNumbers.ToString());
 }
+#region TASK1
 string EnterNumber()
 {   
     string? num = Console.ReadLine(); 
@@ -55,12 +67,34 @@ int SumNumbers(int[] numbersArray) {
     }
     return sum;
 }
-void PrintToConsole(string msg)
-{
-    Console.WriteLine(msg);
+#endregion
+#region TASK2
+
+int[] CreateArray(int lengthArray){
+    Random rnd = new Random();
+    int[] array = new int[lengthArray];
+    for(int i =0; i < lengthArray; i++){
+        array[i] = rnd.Next(99, 1000);
+    }
+    return array;
+}
+int  CountEvenNumbers(int[] array){
+    int count = 0;
+    for(int i =0; i < array.Length; i++){
+        if(array[i] %2 ==0) count++;
+    }
+    return count;
 }
 #endregion
 
+void PrintToConsole(string msg)
+{
+    Console.WriteLine("\n" + msg);
+}
 
-
+void PrintArrayToConsole(int[] array){
+    foreach(int item in array){
+        Console.Write($"{item} ");
+    }
+}
 Main();
