88303e7bee7b0aab033a06202c10677905a9de61
KrSergei <34679066+KrSergei@users.noreply.github.com>
2024-02-19 01:26:49 +0300

    Added Lesson4:Task4-1

---
+++ Lesson4/Task4-1/Program.cs
@@ -0,0 +1,42 @@
+﻿void Main(){
+    string str = GetString();
+    int[] numbersArray = new int[CheckCountNumbersInString(str)];
+    numbersArray = GetNumbersArrayFromString(str);
+    PrintArray(numbersArray);
+}
+
+string GetString(){
+    return Console.ReadLine();
+}
+
+int CheckCountNumbersInString(string str){
+    int count = 0;
+    foreach(char c in str){
+        if(CheckIsnumber(c)) count++;
+    }
+    return count;
+}
+bool CheckIsnumber(char c){
+     if(char.IsDigit(c)) return true;
+     else return false;
+}
+
+int[] GetNumbersArrayFromString(string str){
+    string strNumbers = "";
+    foreach(char c in str){
+        if(CheckIsnumber(c)) strNumbers = strNumbers + c;
+    }
+    int[] numbers = new int[strNumbers.Length];
+    for(int i = 0; i < strNumbers.Length; i++){
+        if(CheckIsnumber(strNumbers[i])) numbers[i] = Convert.ToInt32(strNumbers[i] - '0');
+    } 
+    return numbers;
+}
+
+void PrintArray(int[] array){
+    foreach(int n in array){
+        Console.Write($"{n} ");
+    }
+}
+
+Main();
